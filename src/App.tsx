ca8a9a6c22da3939c1e82fa365c8833a24992f8f
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2023-02-19 16:47:02 +0200

    add icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '@screens/HomeScreen';
+import HomeIcon from '@shared/ui/icons/HomeIcon';
+import PlusCircleIcon from '@shared/ui/icons/PlusCircleIcon';
+import UserIcon from '@shared/ui/icons/UserIcon';
 import { Box } from '@shared/ui/primitives';
 import React from 'react';
 import { useColorScheme } from 'react-native';
@@ -15,16 +18,33 @@ function App(): JSX.Element {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
-        tabBarStyle: { position: 'absolute', backgroundColor: 'transparent' },
+        tabBarStyle: {
+          position: 'absolute',
+          backgroundColor: 'transparent',
+          borderTopWidth: 0,
+          elevation: 0,
+        },
         tabBarActiveTintColor: 'white',
-        // tabBarBackground: () => (
-        //   <Box flex={1} style={{ backgroundColor: '#ff00ff' }} />
-        // ),
+        tabBarLabelStyle: {
+          fontWeight: '500',
+        },
       }}
     >
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Create" component={EmptyScreen} />
-      <Tab.Screen name="Profile" component={EmptyScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ tabBarIcon: HomeIcon }}
+      />
+      <Tab.Screen
+        name="Create"
+        component={EmptyScreen}
+        options={{ tabBarIcon: PlusCircleIcon }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={EmptyScreen}
+        options={{ tabBarIcon: UserIcon }}
+      />
     </Tab.Navigator>
   );
 }
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,15 +1,26 @@
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import { Box, Text } from '@shared/ui/primitives';
 import { normalize } from '@shared/utils';
-import { FlashList, ListRenderItemInfo } from '@shopify/flash-list';
+import {
+  FlashList,
+  FlashListProps,
+  ListRenderItemInfo,
+} from '@shopify/flash-list';
 import React, { useCallback, useState } from 'react';
-import { Dimensions } from 'react-native';
+import { Dimensions, Platform, StatusBar } from 'react-native';
 import FastImage from 'react-native-fast-image';
 import LinearGradient from 'react-native-linear-gradient';
+import Animated, {
+  interpolateColor,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import tinycolor from 'tinycolor2';
 
 const dimensions = Dimensions.get('window');
+
+type HSLColor = [number, number, number];
 
 interface NFT {
   title: string;
@@ -17,7 +28,11 @@ interface NFT {
   username: string;
   imgUrl: string;
   hints: string[];
-  bgColor: string;
+  bgColor: HSLColor;
+}
+
+function hslFromArray([h, s, l]: HSLColor): string {
+  return `hsl(${h}, ${s}%, ${l}%)`;
 }
 
 const DATA: NFT[] = [
@@ -27,7 +42,7 @@ const DATA: NFT[] = [
     title: 'Solana Monkey Business',
     imgUrl: 'https://cdn.solanamonkey.business/gen2/4705.png',
     hints: ['collection', '#4705'],
-    bgColor: '#F299AF',
+    bgColor: [345, 77, 77],
   },
   {
     avatarUrl:
@@ -36,7 +51,7 @@ const DATA: NFT[] = [
     title: 'y00t',
     imgUrl: 'https://metadata.y00ts.com/y/4035.png',
     hints: ['collection', '#4035'],
-    bgColor: '#f2ebd8',
+    bgColor: [44, 50, 90],
   },
   {
     avatarUrl:
@@ -45,103 +60,167 @@ const DATA: NFT[] = [
     title: 'DeGods',
     imgUrl: 'https://metadata.degods.com/g/3324-dead.png',
     hints: ['collection', '#3324'],
-    bgColor: '#f6f2f8',
+    bgColor: [280, 30, 96],
   },
 ];
 
+const AnimatedFlashList =
+  Animated.createAnimatedComponent<FlashListProps<NFT>>(FlashList);
+
 const HomeScreen = () => {
+  const statusBarRef = React.useRef<StatusBar>(null);
   const tabBarHeight = useBottomTabBarHeight();
   const insets = useSafeAreaInsets();
 
   const [items, setItems] = useState(DATA);
 
-  const renderItem = useCallback(({ item }: ListRenderItemInfo<NFT>) => {
-    return (
-      <Box
-        // width={dimensions.width}
-        height={dimensions.height - tabBarHeight}
-        style={{
-          paddingTop: insets.top + normalize(16),
-          backgroundColor: tinycolor(item.bgColor)
-            .darken(30)
-            .desaturate(30)
-            .toHexString(),
-        }}
-        padding={3}
-      >
+  const cardOffset =
+    Platform.select({
+      ios: normalize(16),
+      android: StatusBar.currentHeight,
+    }) || 0;
+
+  const renderItem = useCallback(
+    ({ item }: ListRenderItemInfo<NFT>) => {
+      return (
         <Box
-          flex={1}
-          borderRadius={20}
-          padding={5}
-          overflow="hidden"
+          height={dimensions.height - tabBarHeight}
           style={{
-            backgroundColor: item.bgColor,
+            paddingTop: insets.top + cardOffset,
           }}
+          padding={3}
         >
-          <FastImage
-            resizeMode="contain"
-            style={{ flex: 1 }}
-            source={{
-              uri: item.imgUrl,
-            }}
-          />
-          <LinearGradient
-            colors={['rgba(0,0,0,0.35)', 'rgba(0,0,0,0)']}
+          <Box
+            flex={1}
+            borderRadius={20}
+            padding={5}
+            overflow="hidden"
             style={{
-              top: 0,
-              bottom: 0,
-              left: 0,
-              right: 0,
-              position: 'absolute',
+              backgroundColor: hslFromArray(item.bgColor),
             }}
-          />
-          <Box position="absolute" top={20} left={20}>
-            <Box marginBottom={2} flexDirection="row" alignItems="center">
-              <FastImage
-                style={{
-                  width: 32,
-                  height: 32,
-                  borderRadius: 100,
-                  marginRight: 4,
-                }}
-                source={{ uri: item.avatarUrl }}
-              />
-              <Text fontSize={normalize(12)}>{item.username}</Text>
+          >
+            <FastImage
+              resizeMode="contain"
+              style={{ flex: 1 }}
+              source={{
+                uri: item.imgUrl,
+              }}
+            />
+            <LinearGradient
+              colors={['rgba(0,0,0,0.35)', 'rgba(0,0,0,0)']}
+              style={{
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                position: 'absolute',
+              }}
+            />
+            <Box position="absolute" top={20} left={20}>
+              <Box marginBottom={2} flexDirection="row" alignItems="center">
+                <FastImage
+                  style={{
+                    width: 32,
+                    height: 32,
+                    borderRadius: 100,
+                    marginRight: 4,
+                  }}
+                  source={{ uri: item.avatarUrl }}
+                />
+                <Text fontSize={normalize(12)}>{item.username}</Text>
+              </Box>
+              <Box flexDirection="row" gap={1} marginBottom={2}>
+                {item.hints.map((hint, index) => (
+                  <Box
+                    key={index}
+                    borderWidth={1}
+                    borderRadius={14}
+                    borderColor="primaryText"
+                    paddingHorizontal={2}
+                    paddingVertical={1}
+                  >
+                    <Text>{hint}</Text>
+                  </Box>
+                ))}
+              </Box>
+              <Text fontSize={normalize(24)}>{item.title}</Text>
             </Box>
-            <Box flexDirection="row" gap={1} marginBottom={2}>
-              {item.hints.map((hint) => (
-                <Box
-                  borderWidth={1}
-                  borderRadius={14}
-                  borderColor="primaryText"
-                  paddingHorizontal={2}
-                  paddingVertical={1}
-                >
-                  <Text>{hint}</Text>
-                </Box>
-              ))}
-            </Box>
-            <Text fontSize={normalize(24)}>{item.title}</Text>
           </Box>
         </Box>
-      </Box>
-    );
-  }, []);
+      );
+    },
+    [cardOffset, insets.top, tabBarHeight]
+  );
+
+  const bgColorFromTo = useSharedValue([
+    'transparent',
+    items[0].bgColor,
+    items[1].bgColor,
+  ] as ('transparent' | HSLColor)[]);
+
+  const fullscreenCardHeight = dimensions.height - tabBarHeight;
+
+  const progress = useSharedValue(0);
+
+  const reduceLightning = (hsl: HSLColor | 'transparent') => {
+    'worklet';
+    if (hsl === 'transparent') return hsl;
+    const [h, s, l] = hsl;
+    const sFix = Math.max(0, s - 40);
+    const lFix = Math.max(0, l - 30);
+    return `hsl(${h}, ${sFix}%, ${lFix}%)`;
+  };
+
+  const animatedStyle = useAnimatedStyle(
+    () => ({
+      backgroundColor: interpolateColor(
+        progress.value,
+        [-1, 0, 1],
+        [
+          reduceLightning(bgColorFromTo.value[0]),
+          reduceLightning(bgColorFromTo.value[1]),
+          reduceLightning(bgColorFromTo.value[2]),
+        ]
+      ),
+    }),
+    [bgColorFromTo]
+  );
+
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (ev) => {
+      const idx = Math.floor(ev.contentOffset.y / fullscreenCardHeight);
+      bgColorFromTo.value = [
+        items[idx - 1]?.bgColor || 'transparent',
+        items[idx].bgColor,
+        items[idx + 1]?.bgColor || 'transparent',
+      ];
+      progress.value = ev.contentOffset.y / fullscreenCardHeight - idx;
+    },
+  });
 
   return (
-    <FlashList
-      data={items}
-      renderItem={renderItem}
-      estimatedItemSize={dimensions.height - tabBarHeight}
-      // pagingEnabled
-      showsVerticalScrollIndicator={false}
-      bounces={false}
-      decelerationRate="fast"
-      snapToAlignment="start"
-      snapToInterval={dimensions.height - tabBarHeight}
-      onEndReachedThreshold={1}
-      onEndReached={() => setItems([...items, ...DATA])}
-    />
+    <>
+      <StatusBar
+        ref={statusBarRef}
+        backgroundColor="transparent"
+        translucent={true}
+      />
+      <Animated.View style={[{ flex: 1 }, animatedStyle]}>
+        <AnimatedFlashList
+          onScroll={scrollHandler}
+          data={items}
+          renderItem={renderItem}
+          estimatedItemSize={fullscreenCardHeight}
+          showsVerticalScrollIndicator={false}
+          bounces={false}
+          decelerationRate="fast"
+          snapToAlignment="start"
+          snapToInterval={dimensions.height - tabBarHeight}
+          onEndReachedThreshold={1}
+          onEndReached={() => setItems([...items, ...DATA])}
+        />
+      </Animated.View>
+    </>
   );
 };
 
