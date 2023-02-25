43e83137e39abb6f8201d40bb260c4968cd32cd7
Alexey Tsymbal <tsymbal.aleksey@gmail.com>
2023-02-25 15:59:35 +0400

    add example api usage

--- src/screens/ProfileScreen/index.tsx
+++ src/screens/ProfileScreen/index.tsx
@@ -1,11 +1,27 @@
-import { Text } from '@shared/ui/primitives';
+import { Box, Text } from '@shared/ui/primitives';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { For, Show, useComputed } from '@legendapp/state/react';
+import { getUserProfile } from '@screens/ProfileScreen/api';
 
 const ProfileScreen = () => {
+  // based on https://legendapp.com/open-source/state/react-examples/#list-of-messages
+  const { data: userProfile, loading, error } = getUserProfile();
+  const userName = useComputed(() => userProfile?.get()?.name);
+  const likedNfts = useComputed(() => userProfile?.get()?.likedNfts ?? []);
+
   return (
-    <SafeAreaView style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
-      <Text color="black">Profile</Text>
+    <SafeAreaView>
+      <Show if={userName} else={<Text color="black">Loading...</Text>}>
+        <Box>
+          <Text color="black">{userName}</Text>
+          <Box>
+            <For each={likedNfts}>
+              {(nft) => <Text color="black">{nft.title}</Text>}
+            </For>
+          </Box>
+        </Box>
+      </Show>
     </SafeAreaView>
   );
 };
