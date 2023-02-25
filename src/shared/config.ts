8d3e36b58484926bf10c84c8ad6d6dfeeccc6856
Alexey Tsymbal <tsymbal.aleksey@gmail.com>
2023-02-25 15:49:45 +0400

    add config

---
+++ src/shared/config.ts
@@ -0,0 +1,38 @@
+import { environment } from '../../app.json';
+import { Platform } from 'react-native';
+
+type WalletConfig = SeedGeneratedWalletConfig;
+
+interface SeedGeneratedWalletConfig {
+  type: 'seed-generated';
+  seed: string;
+}
+
+interface AppConfig {
+  baseUrl: string;
+  wallet: WalletConfig;
+}
+
+function getConfig(): AppConfig {
+  switch (environment) {
+    case 'production':
+    case 'development':
+      return {
+        ...Platform.select({
+          ios: require('../../config/env-production.ios.json'),
+          android: require('../../config/env-production.android.json'),
+        }),
+        environment,
+      };
+    default:
+      return {
+        ...Platform.select({
+          ios: require('../../config/env-local-development.ios.json'),
+          android: require('../../config/env-local-development.android.json'),
+        }),
+        environment,
+      };
+  }
+}
+
+export const config = getConfig();
