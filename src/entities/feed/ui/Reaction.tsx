c65e0e9cc61742ddc742bb365f0a351fe0c02d94
pkrvs <uxpkrvs@gmail.com>
2023-03-06 15:38:36 +0300

    fz-reacts-fix

--- src/entities/feed/ui/Reaction.tsx
+++ src/entities/feed/ui/Reaction.tsx
@@ -1,5 +1,6 @@
 import { Box, Text } from '@shared/ui/primitives';
 import { TouchableOpacity } from '@shared/ui/touchables';
+import { scale } from '@shared/utils';
 
 interface ReactionProps {
   kind: string;
@@ -18,15 +19,22 @@ const Reaction: React.FC<ReactionProps> = ({
   return (
     <TouchableOpacity
       flexDirection="row"
+      alignItems="center"
       borderRadius={4}
       borderWidth={1}
       style={{ borderColor: selected ? 'white' : 'rgba(255,255,255,0.12)' }}
-      p={1}
+      px={2}
+      py={1}
       backgroundColor={selected ? 'white' : 'transparent'}
       onPress={onPress}
     >
       <Text>{kind}</Text>
-      <Text fontWeight="500" color={selected ? 'black' : 'white'}>
+      <Text
+        fontWeight="500"
+        marginLeft={1}
+        fontSize={scale(14)}
+        color={selected ? 'black' : 'white'}
+      >
         {simplifiedCounter}
       </Text>
     </TouchableOpacity>
